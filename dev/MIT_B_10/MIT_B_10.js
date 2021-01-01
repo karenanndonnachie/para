2e72c76730b199cc53fc6ebd58dd4c8f5cd6ee61
karenanndonnachie <karenanndonnachie@gmail.com>
2021-01-01 23:53:34 +1100

    Add files via upload

---
+++ dev/MIT_B_10/MIT_B_10.js
@@ -0,0 +1,243 @@
+var font, font2, middle, para, left, topMargin, right, wordsPlaced, prevwordsPlaced, wordpoly, wWidth, queryString;
+var beep, bop, first, sent, timer, columns, pages, polySynth, played, syll, allFonts, numPages, pageNum, params, r, chapters;
+var myText="";
+var go = false;
+var toggle=false;
+var fontSizes = [18, 48, 72]; //16, 24,
+var baseFontSize;
+var toPrint=false;
+var list=[];
+var allSentences=[];
+var vertices = [];
+var allWords=[];
+var sentimentRef, sentVal;
+var shortest= 6;
+var info = "";
+var corpus = ["data/books/austen-emma.txt", "data/books/moby_short.txt", "data/books/austen-sense.txt", "data/books/blake-poems.txt", "data/books/whitman-leaves.txt", "data/books/shakespeare-macbeth.txt"];
+var titles = ["Jane Austen", "Moby Dick", "Sense & Sensibility", "William Blake Poetry", "Walt Whitman", "Macbeth"] ;
+function preload () {
+  list = loadStrings("data/books/moby_short.txt");
+  font=loadFont('data/fonts/HelveticaNowDisplay.otf');
+  font2=loadFont('data/NewRocker-Regular.ttf');
+  sentimentRef = loadJSON('data/sentiment/MIT_afinn_165.json');
+  sentimentRef2 = loadJSON('data/sentiment/vader.json');
+}
+
+function setup() {
+  let params = getURLParams();
+  if (params.seed > 0 && params.page > 0) {
+    randomSeed(params.seed);
+    pageNum=params.page;
+    toPrint=true;
+    if (pageNum==1){
+    createCanvas(2412, 3074);
+    }
+    else if (5>=pageNum>1){
+      createCanvas(4824, 3074);
+    }}
+  else{createCanvas(windowWidth, windowHeight);}
+  frameRate(30);
+  go=false;
+  allFonts=[font, font2];
+  numPages=[1, 2];
+  pages=random(numPages);
+  baseFontSize = random(fontSizes);
+  textFont(random(allFonts));
+  columns=random([1, 2, 4]);
+  textAlign(LEFT, TOP);
+  polySynth = new p5.PolySynth();
+  //list = loadStrings(random(corpus), newText);
+  drawPageSpace();
+}
+
+function draw() {
+  if (go==false) {
+    ellipse(width/2, height/2, 50, 50);
+  } else { 
+    background(215);
+    topMargin = 40;
+    right = width-40;
+    drawPageSpace();
+    wordsPlaced=0;
+    allWords.forEach(each => {
+      each.showWord();
+      wordsPlaced++;
+    } 
+    );
+    if (toggle==true) {
+      if (info[0]=='[') {
+        push();
+        textSize(24);
+        textAlign(LEFT, TOP);
+        fill(255);
+        rect (40, height-44, textWidth(info), 36);
+        fill(0);
+        text(info.toString(), 40, height-44);
+        text(chapters+' / '+frameRate(), 40, height-88);
+        pop();
+      }
+      }
+  }
+}
+
+function drawPageSpace() {
+  if (go==false) {
+    ellipse(width/2, height/2, 50, 50);
+  } else {
+    middle = int(width/2);
+    if (pages==2) {
+      left = middle + 40;
+    } else if (pages==1) {
+      background(215);
+      left = 40;
+    } else {
+      left = random(40, width-200);
+    }
+  }
+}  
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  drawPageSpace();
+  setText(list);
+  //generateVertices(width/4, height/4, wordsPlaced);
+  //setIllust();
+}
+function mousePressed() {
+  //only draw if user has clicked
+  if (go == false) {    
+    go=!go;
+      redraw();
+    //list = loadStrings(random(corpus), newText);
+  }
+  drawPageSpace();
+}
+// This keyTyped function 
+function keyTyped() {
+  //loop();
+  go=true;
+  switch(key) {
+  case '1':
+    frameCount=0;
+    pages=1;
+    loadStrings("data/books/milton-paradise.txt", newText);
+    break;
+  case '3':
+    frameCount=0;
+    pages=1;
+    //myText="";
+    loadStrings(random(corpus), newText);
+    break;
+  case '4':
+    frameCount=0;
+    pages=2;
+    let next=random(corpus);
+    console.log(next);
+    loadStrings(next, newText);
+    break;
+  case '5':
+    bgcol= [255, 5];
+    break;
+  case 'c':
+    pages=1;
+    break;
+  case 'v':
+    toggle=!toggle;
+    break;
+  case 'x':
+    pages=2;
+    break;
+  case 'b':
+    toggle=!toggle;
+    break;
+  }
+}
+
+function newText(result) {
+  background(215);
+  baseFontSize=random(fontSizes);
+  textSize(baseFontSize);
+  textFont(random(allFonts));
+  pages=int(random(1, 3));
+  columns=int(random(1, 4));
+  list=[];
+  var randomselection = random(result.length-8);
+  info=result[0];
+  chapters=randomselection;
+  list=subset(result, randomselection, randomselection+8) ;
+  setText(list);
+}
+
+function setText(list) {
+  allSentences=[];
+  para=[];
+  para = RiTa.splitSentences(list.join(' ').toString());
+  for (var p=0; p<para.length; p++) {
+    allSentences.push(new Paras(p, para[p], info));
+  }
+  allWords=[];
+  allSentences.forEach(sent => {
+    sent.setval(columns, pages);
+  }
+  );
+  frameCount=0;
+}
+
+function playSynth(tone, size, syll) {
+  let dur = 3.0 * size;
+  // time from now (in seconds)
+  let time = 0;
+  // volume, from 0 to 1
+  let vol = 0.1;
+  if (tone==1) {
+    polySynth.play('G2', vol*3, 0, dur);
+    polySynth.play('C3', vol*3, time += 1/4, dur);
+    polySynth.play('G3', vol*3, time += 1/4, dur);
+    //currfreq=60;
+  } else if (tone==2) {
+    polySynth.play('B4', vol*3, 0, dur);
+    polySynth.play('C5', vol*3, time += 1/4, dur);
+    polySynth.play('A4', vol*3, time += 1/4, dur);
+    //currfreq=60;
+  } else if (tone==3) {
+    polySynth.play(midiToFreq(64-syll), 0.2, 0, syll/8 );
+  }
+}
+
+function setIllust() {
+  if (wordsPlaced !== prevwordsPlaced) {
+    generateVertices(width/4, height/4, wordsPlaced);
+  }
+  push();
+  fill(100);
+  stroke(0, 0, 255);
+  strokeWeight(3);
+  translate(width / 4, height /2);
+  beginShape();
+  for (let i = 0; i < vertices.length; i++) { // loop for each point in the current shape.
+    //console.log(vertices[i]);
+    vertex(vertices[i].x, vertices[i].y); // as a vertex with the point coordinates.
+  }
+  endShape(CLOSE);
+  pop();
+  wordpoly=new Polygon();
+  wordpoly.x = width/4;
+  wordpoly.y = height/2;
+  wordpoly.size = 300;
+  wordpoly.sides = wordsPlaced;
+  wordpoly.color = 'darkgray';
+  wordpoly.spin = 0;
+  prevwordsPlaced = wordsPlaced;
+}
+
+function generateVertices(minSize, maxSize, numpoints) { // the minSize is only there to prevent two verticies opposite eachother ending up too close since those shapes looks very odd.
+  push();
+  vertices = [];
+  translate(width/2, height/2);
+  let spread = 2*PI/(numpoints+1); // divide 360 deg (in radians) on the amount of points to later spread the points out. The +1 is used to prevent generating to angles withing the first section when looping, which can cause crossing of lines. The whole generation process can be done in "more random" ways, but this is one of the simpler.
+  for (let i = 0; i < numpoints; i++) { // repeat a amount of times equal to the amount of points we want.
+    vertices.push(p5.Vector.fromAngle(random(i*spread, (i+1)*spread)).mult(random(minSize, maxSize)));
+    // generate an angle within the first section of the rotation, then create a vector with a length of 1 pointing that direction and multiply it by a random number before adding it to a array.
+  }
+  pop();
+}
---
+++ dev/MIT_B_10/Paras.js
@@ -0,0 +1,100 @@
+class Paras {
+  constructor(id, sentence, info) {
+    this.id = id;
+    this.sentence = sentence;
+    this.words = sentence.split(' ');
+    this.newstatus = 0;
+    this.scorepos=0;
+    this.scoreneg=0;
+    this.info=info;
+  }
+  setval(columns, pages){
+    if (pages==1){left=40;}
+    else if (pages==2){left=middle +40;}
+    for (var l=0; l<this.words.length; l++){ 
+       var word = this.words[l];
+       var lcword = word.toLowerCase();
+        if (left + textWidth(word) < right) { 
+          if (topMargin + baseFontSize + baseFontSize/3< height-40) {
+          //text (word, left, topMargin);  
+          if (sentimentRef.hasOwnProperty(lcword)){
+           sentVal=sentimentRef[lcword];
+         }
+         else if (sentimentRef2.hasOwnProperty(lcword)){
+           sentVal=sentimentRef2[lcword];
+         }
+         else {sentVal=0;}
+         if (pageNum>0){timer=1;}
+         else {timer=allWords.length*16;}
+           //timer = 1;
+           syll=RiTa.getSyllables(RiTa.stripPunctuation(word)).length;
+           allWords.push(new Word(word, left, topMargin, textWidth(word), sentVal, timer, syll));
+           left += textWidth(word)+ 5;
+        }}
+        else {
+          if (pages==2) {
+            left = middle+40;}
+          else {
+            left = random(0, middle) + 40;}
+            topMargin += baseFontSize + baseFontSize/3;
+          l--;
+        }
+      }   
+      left = middle + 40;
+      topMargin += baseFontSize +baseFontSize/3; 
+  }
+  display() {}
+}
+
+class Word{
+  constructor(word, left, topMargin, wWidth, sentVal, timer, syll){
+    this.left=left;
+    this.topMargin=topMargin;
+    this.wWidth=wWidth;
+    this.sentVal = sentVal;
+    this.word=word;
+    this.timer=timer;
+    this.played=false;
+    this.syll=syll;
+  }
+  showWord(){
+    if (this.played==true || this.timer<frameCount){
+      fill(255);
+      noStroke();
+      rect (this.left, this.topMargin+baseFontSize/6, this.wWidth, baseFontSize);  
+      if (this.sentVal>0){
+        fill(0,0,255,35);
+        ellipse(this.left+this.wWidth/2,this.topMargin+baseFontSize*5/8,75*this.sentVal, 75*this.sentVal);
+        for (r=75*this.sentVal; r<100*this.sentVal; r++){
+            fill(0,0,255,8);
+            if (this.played!==true){
+            playSynth(2, abs(this.sentVal), 1);}
+            this.played=true;
+            ellipse(this.left+this.wWidth/2,this.topMargin+baseFontSize*5/8,r, r);
+           }
+      }
+    else if (this.sentVal<0){
+      fill(255,0,0,25);
+      let min=75*abs(this.sentVal);
+      rect(this.left,this.topMargin+baseFontSize/6, min, min);
+      for (r=min; r<100*abs(this.sentVal); r++){
+            fill(255, 0, 0, 8);
+            rect(this.left, this.topMargin+baseFontSize/6, r, r);
+      }
+      if (this.played!==true){
+            playSynth(1, abs(this.sentVal), 1);
+          }
+            this.played=true;
+       }
+     else if (this.played!==true){
+            playSynth(3, 1, this.syll);
+          }
+      this.played=true;   
+    if (toggle==true){
+      fill(0);
+      text (this.word, this.left, this.topMargin);
+    }
+    fill(0);
+   } 
+  }
+}
