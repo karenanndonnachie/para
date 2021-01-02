11c423e983654f6a365da4f45c2369ea24e048d5
karenanndonnachie <karenanndonnachie@gmail.com>
2021-01-02 13:20:01 +1100

    Update Paras.js

--- dev/MIT_B_9/Paras.js
+++ dev/MIT_B_9/Paras.js
@@ -26,7 +26,7 @@ class Paras {
            timer=allWords.length*20;
            //timer = 1;
            syll=RiTa.getSyllables(RiTa.stripPunctuation(word)).length;
-           let lex=RiTa.pos(lcword.toString());
+           let lex=new RiString(lcword);
            allWords.push(new Word(word, left, topMargin, textWidth(word), sentVal, timer, syll, lex.pos()));
            left += textWidth(word)+ 5;
         }}
